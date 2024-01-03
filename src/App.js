fc09e05ac8c59dfa88be845286d467e2bb96f847
Lekha <lekhasoni001@gmail.com>
2024-01-04 01:38:59 +0530

    passing object as prop

--- src/App.js
+++ src/App.js
@@ -11,13 +11,16 @@ function App() {
     return(
       <Card
       key = {item.id}
-      img = {item.coverImg}
-      rating = {item.stats.rating}
-      reviewCount = {item.stats.reviewCount}
-      country = {item.location}
-      title = {item.title}
-      price = {item.price}
-      openSpots = {item.openSpots}
+      item = {item}
+      //to avoid clutter pass the entire item object
+
+      // img = {item.coverImg}
+      // rating = {item.stats.rating}
+      // reviewCount = {item.stats.reviewCount}
+      // country = {item.location}
+      // title = {item.title}
+      // price = {item.price}
+      // openSpots = {item.openSpots}
     />
 
     )
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,10 @@ import star from '../images/star.png'
 
 export default function Card(props){
     let badgeText
-    if(props.openSpots === 0){
+    if(props.item.openSpots === 0){
         badgeText = "SOLD OUT"
     }
-    else if(props.country === "Online"){
+    else if(props.item.location === "Online"){
         badgeText = "ONLINE"
     }
         
@@ -16,16 +16,17 @@ export default function Card(props){
         
         {badgeText && <div className="card--badge">{badgeText}</div>}
 
-        <img src={require(`../images/${props.img}`)} alt={props.img} className="card--image" />
+        <img src={require(`../images/${props.item.coverImg}`)} alt={props.item.coverImg} className="card--image" />
         <div className="card--stats">
             <img src={star} className="card--star" />
-            <span>{props.rating} </span>
-            <span className="gray"> {props.reviewCount} . </span>
-            <span className="gray">{props.country}</span>
+            <span>{props.item.stats.rating} </span>
+            <span className="gray">({props.item.stats.reviewCount}) . </span>
+            <span className="gray">{props.item.location}</span>
         </div>
-        <p>{props.title}</p>
+        <p>{props.item.title}</p>
 
-        <p><span className="bold">From ${props.price}</span> / person</p>
+        <p><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
+
